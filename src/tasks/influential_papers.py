0532b4b40b8e962a66d16ef0db98ff50fb30fb91
Andreas Varvarigos <45434379+varvarigos@users.noreply.github.com>
2025-03-08 04:20:43 -0500

    Update influential_papers.py

--- src/tasks/influential_papers.py
+++ src/tasks/influential_papers.py
@@ -12,8 +12,11 @@ Usage:
     The script reads configuration from a YAML file, loads a citation graph from a GEXF file, and prints the K most influential papers.
 """
 
+import datetime
+import re
+
 def influential_papers(message, graph):
-    K = int(message.split("Number of papers to consider: ")[1])
+    K = int(message.split(": ")[1])
     
     in_degree = dict(graph.in_degree())
     sorted_in_degree = sorted(in_degree.items(), key=lambda x: x[1], reverse=True)
@@ -26,5 +29,11 @@ def influential_papers(message, graph):
 
     resp = "Here are the most influential papers:\n"
     for i, paper in enumerate(most_cited_papers):
-        resp += f"{i+1}. Title: {paper['title']}\nAbstract: {paper['abstract']}\n"
+        paper_id = paper['label']
+        paper_id = re.sub(r'[a-zA-Z/]+', '', paper_id)
+        year = paper_id[:2]
+        year = '19' + year if int(year) > 70 else '20' + year
+        month = datetime.date(1900, int(paper_id[2:4]), 1).strftime('%B')
+        
+        resp += f"{i+1}. Title: {paper['title']}, arxiv, {month} {year} \nAbstract: {paper['abstract']}\n"
     return resp
