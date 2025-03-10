f140620b4bf020bf829f0d3d59af09c99b20c47f
Andreas Varvarigos <45434379+varvarigos@users.noreply.github.com>
2025-03-10 03:38:45 -0400

    Update paper_retrieval.py

--- src/tasks/paper_retrieval.py
+++ src/tasks/paper_retrieval.py
@@ -10,29 +10,12 @@ Returns:
 def paper_retrieval(usr_input, template, has_predefined_template=False):
     instruction = "Please select the paper that is more likely to be cited by paper A from candidate papers."
 
-    title_1 = usr_input.split("Title of the Paper A: ")[1].split("\n\n")[0]
-    abstract_1 = usr_input.split("Abstract of Paper A: ")[1].split("\n\n")[0]
-    
-    candidate_papers = []
-    for i in range(1, len(usr_input.split("Candidate "))-1):
-        candidate_papers.append(usr_input.split("Candidate " + str(i) + ": ")[1].split("\n\n")[0])
-    candidate_papers.append(usr_input.split("Candidate " + str(len(usr_input.split("Candidate "))-1) + ": ")[1])
-
-    prompt_input = ""
-    prompt_input = prompt_input + "Title of the Paper A: " + title_1 + "\n"
-    prompt_input = prompt_input + "Abstract of the Paper A: " + abstract_1 + "\n"
-    prompt_input = prompt_input + "candidate papers: " + "\n"
-    for i in range(len(candidate_papers)):
-        prompt_input = prompt_input + str(i) + '. ' + candidate_papers[i] + "\n"
-
-    prompt_input = prompt_input + "Give me the title of your selected paper."
-
     if has_predefined_template:
         res = [
             {"role": "system", "content": instruction},
-            {"role": "user", "content": prompt_input},
+            {"role": "user", "content": usr_input},
         ]
     else:
-        res = template["prompt_input"].format(instruction=instruction, input=prompt_input)
+        res = template["prompt_input"].format(instruction=instruction, input=usr_input)
 
     return res
