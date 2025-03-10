fa840ee235d9fab3f53f4c00c874dffeb766c75a
Andreas Varvarigos <45434379+varvarigos@users.noreply.github.com>
2025-03-10 03:38:29 -0400

    Update link_pred.py

--- src/tasks/link_pred.py
+++ src/tasks/link_pred.py
@@ -12,25 +12,12 @@ Returns:
 def link_pred(usr_input, template, has_predefined_template=False):
     instruction = "Determine if paper A will cite paper B."
 
-    title_A = usr_input.split("Title A: ")[1].split("\n\n")[0]
-    abstract_A = usr_input.split("Abstract A: ")[1].split("\n\n")[0]
-    title_B = usr_input.split("Title B: ")[1].split("\n\n")[0]
-    abstract_B = usr_input.split("Abstract B: ")[1]
-
-    prompt_input = ""
-    prompt_input = prompt_input + "Title of Paper A: " + title_A + '\n'
-    prompt_input = prompt_input + "Abstract of Paper A: " + abstract_A + '\n'
-    prompt_input = prompt_input + "Title of Paper B: " + title_B + '\n'
-    prompt_input = prompt_input + "Abstract of Paper B: " + abstract_B + '\n'
-
-    prompt_input = prompt_input + " Give me a direct answer of yes or no."
-    
     if has_predefined_template:
         res = [
             {"role": "system", "content": instruction},
-            {"role": "user", "content": prompt_input},
+            {"role": "user", "content": usr_input},
         ]
     else:
-        res = template["prompt_input"].format(instruction=instruction, input=prompt_input)
+        res = template["prompt_input"].format(instruction=instruction, input=usr_input)
 
     return res
