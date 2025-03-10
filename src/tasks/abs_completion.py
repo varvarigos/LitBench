655cd51ac5d6258974fa282136fa30863f9c7da1
Andreas Varvarigos <45434379+varvarigos@users.noreply.github.com>
2025-03-10 03:11:11 -0400

    Update abs_completion.py

--- src/tasks/abs_completion.py
+++ src/tasks/abs_completion.py
@@ -15,7 +15,7 @@ Returns:
     str: A formatted string with the instruction and the input embedded in the template.
 """
 
-def abs_completion(usr_input, template):
+def abs_completion(usr_input, template, has_predefined_template=False):
     instruction = "Please complete the abstract of a paper."
 
     title = usr_input.split("Title: ")[1].split("\n\n")[0]
@@ -24,6 +24,13 @@ def abs_completion(usr_input, template):
     prompt_input = ""
     prompt_input = prompt_input + "Title: " + title + "\n"
     prompt_input = prompt_input + "Part of abstract: " + abstract
-    res = template["prompt_input"].format(instruction=instruction, input=prompt_input)
+
+    if has_predefined_template:
+        res = [
+            {"role": "system", "content": instruction},
+            {"role": "user", "content": prompt_input},
+        ]
+    else:
+        res = template["prompt_input"].format(instruction=instruction, input=prompt_input)
 
     return res
